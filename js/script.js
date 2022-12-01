05abc38e02f7cbe48e9924d36a902d9154c42f61
Felilpz <luizfelipe.vieira12@gmail.com>
2022-12-01 16:50:33 -0300

    html css feito, falta js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Seleção de elementos
+const todoForm = document.querySelector('#tarefa-formulario')
+const todoInput = document.querySelector('#tarefa-formulario')
+const todoList = document.querySelector('#tarefa-formulario')
+const todoForm = document.querySelector('#tarefa-formulario')
+
+//Funções
+
+
+//Eventos
